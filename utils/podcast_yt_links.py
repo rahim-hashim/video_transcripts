c77d6299ed8b79358656a72e5394e4531d9596eb
Rahim Hashim <rahimh233@gmail.com>
2024-09-25 09:21:08 -0400

    Transcription Update

--- utils/podcast_yt_links.py
+++ utils/podcast_yt_links.py
@@ -16,5 +16,6 @@ podcast_playlist_names = {
   'https://www.youtube.com/playlist?list=PLrxfgDEc2NxY_fRExpDXr87tzRbPCaA5x': 'Mindscape Podcast',
   'https://www.youtube.com/playlist?list=PL6ZR-sD5vIlxemueCxB9HLe9I9_K9l5jQ': 'Empire Podcast',
   'https://www.youtube.com/playlist?list=PLS8aEHTqDvpInY9kslUOOK8Qj_-B91o_W': 'Conversation with Tyler',
-  'https://www.youtube.com/playlist?list=PLTYmWuFco1_1mHkLh_fKG3OFUFYYel1kw': 'Owen Jones - Palestine'
+  'https://www.youtube.com/playlist?list=PLTYmWuFco1_1mHkLh_fKG3OFUFYYel1kw': 'Owen Jones - Palestine',
+  'https://www.youtube.com/playlist?list=PLYoqcr7bAIs7kdyMhh-9m4kPLCG6dGAkX': 'The Master Plan'
 }
